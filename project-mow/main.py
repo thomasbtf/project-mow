d87ff2fa4b318d24b5791e630c278d08cb414381
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-06-08 14:17:20 +0200

    add first main

---
+++ project-mow/main.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+
+from .drivers.l298n import L298N_1_Motor
+
+
+def main():
+    ena = 21
+    in1 = 20
+    in2 = 16
+    enb = 13
+    in3 = 26
+    in4 = 19
+    GPIO.setmode(GPIO.BOARD)
+
+    motor = L298N_1_Motor(en=ena, in1=in1, in2=in2)
+    motor.change_speed(10)
+    motor.forward()
+
+
+if __name__ == "__main__":
+    main()
